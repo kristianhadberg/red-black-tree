79fd95f44efdb9313349a70ef56bba495d4f942d
Kristian <kristianhadberg@gmail.com>
2024-12-29 12:20:58 +0100

    add delete & deleteFixup, tree now recolors & rebalances automatically upon deletion

--- src/red-black-tree.js
+++ src/red-black-tree.js
@@ -103,12 +103,9 @@ export default class RedBlackTree {
   }
 
   insertFixup(violatingNode) {
-    // z == violating node
-    // y == uncle?
     while (violatingNode.parent && violatingNode.parent.color == "RED") {
       // left side cases
       if (violatingNode.parent == violatingNode.parent.parent.left) {
-        console.log("violation");
         const uncle = violatingNode.parent.parent.right;
 
         if (uncle.color == "RED") {
@@ -129,6 +126,7 @@ export default class RedBlackTree {
           this.rotateRight(violatingNode.parent.parent);
         }
       } else {
+        // right side cases, same as the code above just the opposite side
         const uncle = violatingNode.parent.parent.left;
 
         if (uncle.color == "RED") {
@@ -156,113 +154,169 @@ export default class RedBlackTree {
     this.root.color = "BLACK";
   }
 
-  /* 
-    Print function from https://github.com/petlatkea/dsa-starterfiles/blob/main/BinaryTree/script.js
-    used for initial testing
-  */
-  printTree() {
-    // Print the tree in a nice way - by creating a (jagged) 2D array of the tree
-    // each level (starting from root) is an array in the array that doubles in size from the previous level
+  delete(value) {
+    const node = this.search(value);
 
-    // breaks if the tree is too deep - but that's a problem for another day
+    if (node == this.NIL) {
+      console.log(`Value ${value} not found in the tree`);
+      return `Value ${value} not found in the tree`;
+    }
+    let nodeOriginalColor = node.color;
 
-    // Use DFS to fill array with values
-    const treeArray = [];
-    let height = 0; // and while we're at it, calculate the height of the tree
-    buildTreeArray(this.root, 0, 0);
-
-    // Does a Depth-First-Scan of the Tree,
-    // keeping track of the current depth (how far down from the top)
-    // and the current indent (how far right from the (possible) left-most node at this depth)
-    // stores the node values in a 2D array
-    function buildTreeArray(node, depth, indent) {
-      if (!node) {
-        return;
-      }
-      height = Math.max(height, depth);
-      // insert this node value in the 2D array
-      if (!treeArray[depth]) treeArray[depth] = [];
-      treeArray[depth][indent] = node.value;
-      // visit its children - remember to double indent
-      buildTreeArray(node.left, depth + 1, indent * 2);
-      buildTreeArray(node.right, depth + 1, indent * 2 + 1);
+    let nodeToBeFixed;
+    let minimumNodeRightChild;
+    // case 1 left is NIL
+    if (node.left == this.NIL) {
+      nodeToBeFixed = node.right;
+      this.transplant(node, node.right);
     }
 
-    // Apparently I'm not smart enough to calculate these, so here's a pre-calculated list
-    const indentations = [1, 2, 5, 11, 23, 46, 93];
+    // case 2, right is NIL
+    else if (node.right == this.NIL) {
+      nodeToBeFixed = node.left;
+      this.transplant(node, node.left);
+    } else {
+      /* 
+        case 3, neither are NIL
+        find minimum value in the node to be deleted's right sub tree 
+    */
+      const minimumNodeInRightSubTree = this.minimum(node.right);
+      nodeOriginalColor = minimumNodeInRightSubTree.color;
+      minimumNodeRightChild = minimumNodeInRightSubTree.right;
+      nodeToBeFixed = minimumNodeRightChild;
 
-    let treeString = " ";
-    // Display array - one level at a time
-    for (let depth = 0; depth < treeArray.length; depth++) {
-      const values = treeArray[depth];
-
-      // Calculate indent for this depth (or find it in the pre-calculated table)
-      let currentHeight = height - depth; // currentHeight is the distance from the bottom of the tree
-      let indent = indentations[currentHeight];
-
-      // Only display tree structure if we are not at the top
-      if (depth > 0) {
-        // Loop through half the values - and show a subtree with left and right
-        for (let i = 0; i < values.length / 2; i++) {
-          treeString += " ".repeat(indent);
-          // Only show sub-tree if there are some values below
-          if (values[i * 2] != undefined || values[i * 2 + 1] != undefined) {
-            treeString += "┌";
-            treeString += "─".repeat(indent > 1 ? indent : 0);
-            treeString += "┴";
-            treeString += "─".repeat(indent > 1 ? indent : 0);
-            treeString += "┐";
-          } else {
-            treeString += "   " + "  ".repeat(indent > 1 ? indent : 0);
-          }
-          treeString += " ".repeat(indent);
-          // add a single space before the next "block"
-          treeString += " ";
-        }
-        // and finalize the current line
-        treeString += "\n";
-      }
-
-      // Indent numbers one less than their "tree drawings"
-      // Unless it is the first one, then it is two (or maybe three) less ... mystic math!
-      if (depth == 0) {
-        treeString += " ".repeat(indent - 2);
+      // if the minimum node is the direct child of the node to be deleted
+      if (minimumNodeInRightSubTree.parent == node) {
+        minimumNodeRightChild.parent = minimumNodeInRightSubTree;
       } else {
-        treeString += " ".repeat(indent - 1);
+        this.transplant(minimumNodeInRightSubTree, minimumNodeRightChild);
+        minimumNodeInRightSubTree.right = node.right;
+        minimumNodeInRightSubTree.right.parent = minimumNodeInRightSubTree;
       }
 
-      // display values
-      for (let i = 0; i < values.length; i++) {
-        // if both children are undefined, don't show any of then
-        // if only one child is, show it as underscores _
-        const showUndefined =
-          !values[i - (i % 2)] && !values[i - (i % 2) + 1] ? " " : "_";
-        // if depth is lowest (height-1) - pad values to two characters
-        if (depth == height) {
-          treeString += String(values[i] ?? showUndefined.repeat(2)).padStart(
-            2,
-            " "
-          );
-          // and add a single space
-          treeString += " ";
-        } else {
-          // otherwise center values in block of three
-          treeString += String(values[i] ?? showUndefined.repeat(3))
-            .padEnd(2, " ")
-            .padStart(3, " ");
+      this.transplant(node, minimumNodeInRightSubTree);
+      minimumNodeInRightSubTree.left = node.left;
+      minimumNodeInRightSubTree.left.parent = minimumNodeInRightSubTree;
+      minimumNodeInRightSubTree.color = node.color;
+    }
 
-          // and add twice the indentation of spaces + 1 in the middle
-          treeString += " ".repeat(indent - 1);
-          treeString += " ";
-          treeString += " ".repeat(indent - 1);
+    if (nodeOriginalColor == "BLACK") {
+      this.deleteFixup(nodeToBeFixed);
+    }
+  }
+
+  deleteFixup(node) {
+    while (node != this.root && node.color == "BLACK") {
+      // if deleted node is left child
+      if (node == node.parent.left) {
+        let uncle = node.parent.right;
+
+        // case 1 - uncle is red
+        if (uncle.color == "RED") {
+          uncle.color = "BLACK";
+          node.parent.color = "RED";
+          this.rotateLeft(node.parent);
+          uncle = node.parent.right;
+        }
+
+        // case 2 - uncle is black && both uncle's children are black
+        if (uncle.left.color == "BLACK" && uncle.right.color == "BLACK") {
+          uncle.color = "RED";
+          node = node.parent;
+        } else {
+          // case 3 - uncle is black && uncle.left is RED && uncle.right is BLACK
+          if (uncle.right.color == "BLACK") {
+            uncle.left.color = "BLACK";
+            uncle.color = "RED";
+            this.rotateRight(uncle);
+            uncle = node.parent.right;
+          }
+          // case 4 - uncle is black and both children are red (always happens if case 3 happens)
+          uncle.color = node.parent.color;
+          node.parent.color = "BLACK";
+          uncle.right.color = "BLACK";
+          this.rotateLeft(node.parent);
+          node = this.root;
         }
       }
 
-      // finalize the value-line
-      treeString += "\n";
+      // if deleted node is right child - same as the code above, just the opposite side
+      else {
+        let uncle = node.parent.left;
+
+        // case 1
+        if (uncle.color == "RED") {
+          uncle.color = "BLACK";
+          node.parent.color = "RED";
+          this.rotateRight(node.parent);
+          uncle = node.parent.right;
+        }
+
+        // case 2
+        if (uncle.left.color == "BLACK" && uncle.right.color == "BLACK") {
+          uncle.color = "RED";
+          node = node.parent;
+        } else {
+          // case 3 - uncle is black && uncle.left is BLACK && uncle.right is RED
+          if (uncle.left.color == "BLACK") {
+            uncle.right.color = "BLACK";
+            uncle.color = "RED";
+            this.rotateLeft(uncle);
+            uncle = node.parent.leftr;
+          }
+          // case 4
+          uncle.color = node.parent.color;
+          node.parent.color = "BLACK";
+          uncle.left.color = "BLACK";
+          this.rotateRight(node.parent);
+          node = this.root;
+        }
+      }
+    }
+    node.color = "BLACK";
+  }
+
+  /* 
+    Helper function to swap nodes within a subtree
+    Used for deleting nodes
+  */
+  transplant(firstNode, secondNode) {
+    if (firstNode.parent == null) {
+      // firstNode is root
+      this.root = secondNode;
+    } else if (firstNode == firstNode.parent.left) {
+      firstNode.parent.left = secondNode;
+    } else {
+      firstNode.parent.right = secondNode;
+    }
+    secondNode.parent = firstNode.parent;
+  }
+
+  /* 
+    Find the lowest value in the given subtree
+  */
+  minimum(node) {
+    while (node.left != this.NIL) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  /* 
+    Find a specific value in the tree
+  */
+  search(value) {
+    let currentNode = this.root;
+
+    while (currentNode != this.NIL && value != currentNode.value) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
     }
 
-    console.log(treeString);
+    return currentNode;
   }
 
   print(node = this.root, depth = 0) {
@@ -290,54 +344,5 @@ export default class RedBlackTree {
     this.traverse(node.left);
     console.log(node.value);
     this.traverse(node.right);
-  }
-
-  bfs() {
-    if (this.root == null) return;
-
-    const queue = [];
-    queue.push(this.root);
-
-    const list = [];
-
-    while (queue.length > 0) {
-      const item = queue.shift();
-
-      console.log(item);
-      console.log(this.depth(item));
-      list.push({
-        value: item.value,
-        color: item.color,
-        depth: this.depth(item),
-      });
-
-      if (item.left && item.left != this.NIL) queue.push(item.left);
-      if (item.right && item.right != this.NIL) queue.push(item.right);
-    }
-    console.log(list);
-  }
-
-  depth(node) {
-    let depth = 0;
-
-    const queue = [];
-    queue.push(this.root);
-
-    while (queue.length > 0) {
-      const queueLength = queue.length;
-      for (let i = 0; i < queueLength; i++) {
-        const frontNode = queue.shift();
-        if (frontNode == node) {
-          return depth;
-        }
-        if (frontNode.left != null) {
-          queue.push(frontNode.left);
-        }
-        if (frontNode.right != null) {
-          queue.push(frontNode.right);
-        }
-      }
-      depth++;
-    }
   }
 }
